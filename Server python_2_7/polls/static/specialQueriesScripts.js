8a33ad1b01f30d815c139d65f283fd8233e7e790
michael ozeri <ozeri.mich@gmail.com>
2018-01-16 02:30:44 +0200

    added display button for Death / birth button

--- Server python_2_7/polls/static/specialQueriesScripts.js
+++ Server python_2_7/polls/static/specialQueriesScripts.js
@@ -1,18 +1,4 @@
-var match4AllCurrentFilterNum = 0;
-var addedFilters = "";
-var filterTypesChosen = new Array();
-var filterWordsChosen = new Array();
-
-
-
-function createJSONStringforImage(flowname, encoding) {
-    var jsonString = "{";
-    jsonString = addFlowNameJSON(jsonString,flowname);
-    jsonString = addparamsKeyforJSON(jsonString);
-    jsonString = addParamJSON(jsonString,"photo",encoding);
-    jsonString += "]}";
-    return jsonString;
-}
+var DEADButton = 1;
 
 function createTableFromResponse(responseArr) {
     if(responseArr.isError == "true"){
@@ -67,21 +53,28 @@ function fadeOutButtons(elementId, elementId2, elementId3) {
     }
 }
 
+function hideDisplayofClass(classNames) {
+    var fadeArr = document.getElementsByClassName(classNames);
+    for (i = 0; i < fadeArr.length; i++) {
+        fadeArr[i].style.display = "none";
+    }
+}
+
 function loadDocSpecialQuery(postUrl) {
     var sentData = createJSONString(postUrl);
     var xhttp = new XMLHttpRequest();
     // debugger;
     xhttp.onreadystatechange = function() {
         if (this.readyState === 4 && this.status === 200) {
-            debugger;
+            // debugger;
             var responseArr = JSON.parse(this.responseText);
             console.log("initializing table head and opening body tag");
             var finalTable = createTableFromResponse(responseArr);
             fadeInTable(finalTable,"big");
-            // debugger;
             document.getElementById("responseheader").innerText = "the Following year was found:";
-            fadeOutButtons("clearAll", "getSongsButton");
-            document.getElementsByClassName("tofade").style.visibility = "hidden";
+            // debugger;
+            hideDisplayofClass("tofade");
+            hideDisplayofClass("querybutton");
         }
     };
     xhttp.open("POST", "Generic", true);
@@ -90,15 +83,6 @@ function loadDocSpecialQuery(postUrl) {
     xhttp.send("data="+sentData);
 
 }
-
-
-function printArraysTillNow() {
-    for (i = 0; i < match4AllCurrentFilterNum; i++) {
-        console.log("type number " + i + " is: " + filterTypesChosen[i] + ", filter number " + i + " is: " + filterWordsChosen[i]);
-    }
-}
-
-
 
 function addFlowNameJSON(jsonString, flowname) {
     jsonString += "\"flowname\":" + "\"" + flowname + "\",";
@@ -134,16 +118,38 @@ function createJSONString(flowname) {
     return jsonString;
 }
 
-
-function disableSpecialFilters(notDisable){
-    additionalFiltersSelected = notDisable;
-    var specArr = document.getElementsByClassName("spec");
-    // debugger;
-    for(i=0;i<specArr.length;i++){
-        var spec = specArr[i];
-        if(spec.id != notDisable){
-            spec.disabled = "true";
-        }
+function switchDEAD(){
+    if(DEADButton == 1){
+        document.getElementById("DEAD").innerHTML = "<div id=\"responseheader\"></div>\n" +
+            "                    <h4 class=\"tofade\">Death / Birth query:</h4>\n" +
+            "                    <span class=\"tofade\">the year that at least </span>\n" +
+            "                    <span class=\"form-group tofade\" >\n" +
+            "                    <input type=\"text\" class=\"form-control\" value=\"2\" id=\"num\">\n" +
+            "                    </span>\n" +
+            "                    <span class=\"tofade\">artists of genre </span>\n" +
+            "                    <span class=\"form-group tofade\" >\n" +
+            "                    <input type=\"text\" class=\"form-control\" value=\"hip-hop\" id=\"genre\">\n" +
+            "                    </span>\n" +
+            "                    <span class=\"tofade\"> have </span>\n" +
+            "                    <form id=\"Died\" class=\"tofade\">\n" +
+            "                        <div class=\"form-group\">\n" +
+            "                            <select class=\"form-control\" id=\"sel0\">\n" +
+            "                                <option>Died</option>\n" +
+            "                                <option>Born</option>\n" +
+            "                            </select>\n" +
+            "                        </div>\n" +
+            "                    </form>\n" +
+            "\n" +
+            "                    <!--added forms will be here-->\n" +
+            "                    <div id=\"big\"></div>\n" +
+            "\n" +
+            "                    <button id=\"getSongsButton\" class =\"btn btn-default querybutton \" onclick=\"loadDocSpecialQuery('year')\" >Query</button>\n"+
+        "<button id=\"changeDEAD\" class =\"btn btn-default tofade\" onclick=\"switchDEAD()\">close Death / Birth query</button>";
+        DEADButton = 0;
+    }
+    else{
+        document.getElementById("DEAD").innerHTML = "<button id=\"changeDEAD\" class =\"btn btn-default\" onclick=\"switchDEAD()\">open Death / Birth query</button>";
+        DEADButton=1;
     }
 }
 
