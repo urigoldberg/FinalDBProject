fd0fe43d4b8156886b2bf930e87fc346ffdc3c89
Orr Barkat <barkat.orr@gmail.com>
2018-01-16 20:29:08 +0200

    db creation

--- CreateDB/album.sql
+++ CreateDB/album.sql
@@ -17,18 +17,13 @@ CREATE TABLE if NOT EXISTS Album(
   FOREIGN KEY (artist_id) REFERENCES artists(id)
 );
 
-# CREATE TABLE if NOT EXISTS Media(
-#   artist_db_id INT,
-#   album_db_id INT,
-#   song_db_id INT UNIQUE,
-#   media_url VARCHAR(255) NOT NULL  ,
-#   artist_id INT,
-#   album_id INT,
-#   id INT NOT NULL AUTO_INCREMENT,
-#   PRIMARY KEY(id),
-#   FOREIGN KEY (artist_id) REFERENCES artists(id),
-#   FOREIGN KEY (album_id) REFERENCES Album(id)
-# );
+CREATE TABLE if NOT EXISTS Lyrics(
+  lyrics TEXT,
+  song_id INT UNIQUE,
+  id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY(id),
+  FULLTEXT (lyrics)
+)ENGINE = MYISAM;
 
 CREATE TABLE if NOT EXISTS Song(
   db_id INT NOT NULL UNIQUE,
@@ -60,4 +55,30 @@ CREATE TABLE if NOT EXISTS CountryArtists(
   FOREIGN KEY (artist_id) REFERENCES artists(id),
   INDEX CountryArtists(name) USING BTREE,
   UNIQUE unique_index (name, artist_id)
+);
+
+CREATE TABLE if NOT EXISTS Country(
+  name VARCHAR(255) UNIQUE NOT NULL,
+  latitude DOUBLE,
+  longitude DOUBLE,
+  region VARCHAR(255),
+  subregion VARCHAR(255),
+  capital VARCHAR(255),
+  cca3 VARCHAR(3),
+  id INT NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (id),
+  INDEX lat(latitude),
+  INDEX lon(longitude)
+);
+
+CREATE TABLE if NOT EXISTS UserInteraction(
+  user_name VARCHAR(20),
+  song_id INT,
+  created_at TIMESTAMP,
+  id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (user_name) REFERENCES users_table(user_name),
+  FOREIGN KEY (song_id) REFERENCES Song(id),
+  UNIQUE (user_name, song_id),
+  INDEX (user_name)
 );
