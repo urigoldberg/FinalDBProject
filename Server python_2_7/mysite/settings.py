a789ea1d66767f68731c370f732b4264726edc2b
DESKTOP-295IBD0\urigo <urigoldberg050@gmail.com>
2018-01-22 21:38:05 +0200

    debug = false

--- Server python_2_7/mysite/settings.py
+++ Server python_2_7/mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '8bqodq3po+b#!lgrn_=y+b&jj)v6d&63nxchwkjtr51=@ql_fr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
